a3de8cc7197e47c812622ec0140520719ae7cd63
Ufoma Onowho <onowhoufoma@gmail.com>
2026-09-07 19:26:36 +0100

    Prepare backend for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ import ratingRoutes from './routes/ratings.js';
 
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
